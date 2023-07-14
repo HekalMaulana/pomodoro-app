11f0c85bb5aa5c869004c855da7a46be69da98f4
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2023-07-14 15:13:19 +0800

    End of pomodoro app

--- main.py
+++ main.py
@@ -10,14 +10,39 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer_zone = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
+def reset_timer():
+    window.after_cancel(timer_zone)
+    global reps
+    reps = 0
+    canvas.itemconfig(time_label, text=f"00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    checkmark_label.config(text="")
+
+
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    # work_sec = WORK_MIN * 60
+    # short_break_sec = SHORT_BREAK_MIN * 60
+    # long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(10)
+        title_label.config(text="BREAK", fg=RED)
+    elif reps % 2 == 0:
+        count_down(5)
+        title_label.config(text="BREAK", fg=PINK)
+    else:
+        count_down(15)
+        title_label.config(text="WORK", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -25,6 +50,7 @@ def start_timer():
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    global reps
 
     if count_min < 10:
         count_min = f"0{count_min}"
@@ -33,8 +59,16 @@ def count_down(count):
         count_sec = f"0{count_sec}"
 
     canvas.itemconfig(time_label, text=f"{count_min}:{count_sec}")
-    if count > 0:
-        window.after(1000, count_down, count - 1)
+    if count >= 0:
+        global timer_zone
+        timer_zone = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            marks += "✔"
+        checkmark_label.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -50,7 +84,7 @@ canvas.create_image(100, 110, image=tomato_img)
 time_label = canvas.create_text(100, 130, text="00.00", font=(FONT_NAME, 35, "bold"), fill="white")
 canvas.grid(column=1, row=1)
 
-# Timer Label
+# Title Label
 title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50, "bold"), bg=YELLOW, highlightthickness=0)
 title_label.grid(column=1, row=0)
 
@@ -59,11 +93,11 @@ start_button = Button(text="Start", font=(FONT_NAME, 12, "bold"), command=start_
 start_button.grid(column=0, row=2)
 
 # Reset Button
-reset_button = Button(text="Reset", font=(FONT_NAME, 12, "bold"))
+reset_button = Button(text="Reset", font=(FONT_NAME, 12, "bold"), command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 # checkmark label
-checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 18))
+checkmark_label = Label(fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 18))
 checkmark_label.grid(column=1, row=3)
 
 window.mainloop()
